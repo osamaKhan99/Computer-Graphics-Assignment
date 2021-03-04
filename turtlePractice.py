e4c3027db2392d2a5b7ade8b894c1a19156df6e1
Osama Khan <osama021khan@gmail.com>
2021-03-04 23:22:54 +0500

    turtle graphics practice

---
+++ turtlePractice.py
@@ -0,0 +1,82 @@
+# Below all three pattern programs and Famous LOGO have coded, 
+# please comment remaining three before execution of one. Thank You!
+
+import turtle
+ 
+board = turtle.Turtle()
+board.pensize(5)
+
+# Triangles
+
+# board.forward(300)
+ 
+# board.left(120)
+# board.forward(300)
+ 
+# board.left(120)
+# board.forward(150)
+
+# board.left(120)
+# board.forward(150)
+
+# board.right(120)
+# board.forward(150)
+
+# board.right(120)
+# board.forward(150)
+
+# board.left(120)
+# board.forward(150)
+ 
+# turtle.done()
+
+
+
+#Square Pattern
+
+
+# board2 = turtle.Turtle()
+# board2.pensize(5)
+# x=200
+
+# for i in range(8):
+#     board2.forward(x)
+#     board2.left(90)
+#     board2.forward(x)
+#     board2.left(90)
+#     x = x-25
+
+# turtle.done()
+
+#Squares
+
+# board3 = turtle.Turtle()
+# board3.pensize(5)
+# x=100
+
+# for i in range(4):
+#     for j in range(4):
+#         board3.forward(x)
+#         board3.left(90)
+#     x = x-25
+    
+# turtle.done()
+
+
+# Famous LOGO
+
+board4 = turtle.Turtle()
+board4.pensize(5)
+x=50
+
+for i in range(3):
+    board4.forward(x)
+    board4.left(120)
+    board4.forward(x)
+    board4.right(60)
+    board4.forward(x)
+    board4.right(120)
+    board4.forward(x)
+    board4.right(60)
+
+turtle.done()
